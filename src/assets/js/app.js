26b7531682a53835d3cef0f2f0d78895d17fe8a0
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-08-03 09:14:39 -0500

    Mostrar felicitaciones

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,19 +1,21 @@
 "use strict";
+
+const state= {
+    screen : null,
+    user: null,
+    vehicle: null
+};
 const render = (root)=>{
     root.empty();
     const section = $('<section class="components"></section>');
 
     if(state.screen == null){
-        section.append(DniRegister( _ => render(root)));
+        section.append(Shopping_thanks( _ => render(root)));
     }
     else{
         section.append(state.screen( _ => render(root)));
     }
     root.append(section);
-};
-
-const state = {
-    screen : null
 };
 
 $( _=>{
---
+++ src/assets/js/components/mapa.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const Garages = (update) => {
+  const garage= $('<section>Aquí podrás ver todos los talleres!</section>');
+  return garage;
+}
