c6e3a9718da15f187cd411e151db14641bb86236
Fiorellacr24 <fiorella.0624@gmail.com>
2017-08-03 04:01:53 -0500

    add design responsive

--- src/assets/js/components/planes.js
+++ src/assets/js/components/planes.js
@@ -2,51 +2,86 @@
 
 const Planes = ()=>{
     const container = $('<section class="container"></section>');
-    const titleplan = $('<h1>Planes</h1>');
-    const infoplan = $('<p>Por favor, elige el plan que más te convenga:</p>');
+    const titleplan = $('<h3>¡Crea un Plan a tu conveniencia!</h3>');
+    const infoplan = $('<p>Elige el plan que más te guste:</p>');
     const tablero = $('<div class="row"></div>');
 
     $.getJSON('https://rasveuswap01-test01.azurewebsites.net/Laboratoria/v1/plan', (json) => {
         state.plan = json;
         $.each(state.plan, (i,val)=>{
-            const plan = $('<div class="col s12 m4 l4"></div>');
+            const contmobile = $('<div class="hide-on-large-only"></div>');
+            const ul = $('<ul class="collapsible" data-collapsible="accordion">');
+            const li = $('<li></li>');
+            const collap_header = $('<div class="collapsible-header">Plan<br>'+state.plan[i].nombre+'</div>');
+            const collap_body = $('<div class="collapsible-body"></div>');
+            const labelmobile = $('<label for="slider">Número de Cuotas</label>');
+            const rangemobile = $('<input id="range" type="range" value="0" min="0" max="12" step="6"/>');
+            const primamobile = $('<p>S/ 0.00</p>');
+            const buttonmobile = $('<button>Adquirir</button>');
+            
+            
+            const plan = $('<div class="hide-on-small-only col m4 l4"></div>');
             const contenido = $('<h4>'+state.plan[i].nombre+'</h4>');
-            const range = $('<label for="slider">Cuotas:</label><input type="range" value="12" min="0" max="12" step="6"/>');
-            const prima = $('<p>'+state.plan[i].prima+'</p>');
+            const label = $('<label for="slider">Número de Cuotas</label>');
+            const range = $('<input id="range" type="range" value="0" min="0" max="12" step="6"/>');
+            const prima = $('<p>S/ 0.00</p>');
             const button = $('<button>Adquirir</button>');
             
+            let selectrange = $(".value").text();
+            let valueprima = state.plan[i].prima;
+            
+            range.on("change", function(){
+                let operacion = parseInt(valueprima * (this.value));
+                prima.text("S/ "+operacion);
+            });
+            
+            rangemobile.on("change", function(){
+                let operacion = parseInt(valueprima * (this.value));
+                primamobile.text("S/ "+operacion);
+            });
+            
+            $(document).ready(function(){
+                $('.collapsible').collapsible();
+            });
+            
+            li.append(collap_header);
+            collap_body.append(labelmobile);
+            collap_body.append(rangemobile);
+            collap_body.append(primamobile);
+            li.append(collap_body);
+            ul.append(li);
+            contmobile.append(ul);
+            
             plan.append(contenido);
+            plan.append(label);
             plan.append(range);
             plan.append(prima);
-            
-            
+                                         
             $.each(state.plan[i].coberturas, (i,val)=>{
                 const cober = $('<p>'+val.descripcion+'</p>');
+                const cobermobile = $('<p>'+val.descripcion+'</p>');
+                
+                collap_body.append(cobermobile);
+                collap_body.append(buttonmobile);
                 plan.append(cober);
             });
             
             button.on('click', ()=>{
+                state.selectPlan = state.plan[i].id; 
                 state.selectPlan = state.plan[i].prima; 
                 state.selectPlan = state.plan[i];
             });
             
             plan.append(button);
+            tablero.append(contmobile);
             tablero.append(plan);
-        });
-    });
-    /*
-    $.getJSON('http://rasveuswap01-test01.azurewebsites.net/Laboratoria/v1/cobertura', (json) => {
-        state.cobertura = json;
-        $.each(state.cobertura, (i,val)=>{
-            const listcobertura = $('<p>'+state.cobertura[i].id+'</p>');
             
-            tablero.append(listcobertura);
         });
-    });*/
-    
+        
+    });
     container.append(titleplan);
     container.append(infoplan);    
     container.append(tablero);
-
+    
     return container;
 }
